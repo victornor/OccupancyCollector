0534d04d6b07aab07da41fb5064e61bbb28913b1
Victor <1-1@hotmail.dk>
2022-11-12 22:55:38 +0100

    feat: add initial project structure

---
+++ OccupancyCollector.Mobile/macOS/Main.cs
@@ -0,0 +1,15 @@
+using AppKit;
+
+namespace OccupancyCollector.macOS
+{
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.SharedApplication.Delegate = new App();
+			NSApplication.Main(args);  
+		}
+	}
+}
+
